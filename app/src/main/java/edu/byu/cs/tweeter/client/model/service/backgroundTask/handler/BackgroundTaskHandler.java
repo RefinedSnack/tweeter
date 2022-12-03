4202ea31a9bc224890b52a187095dc744033d9d4
RefinedSnack <RefinedSnack@gmail.com>
2022-12-03 16:51:13 -0700

    Further Observer Refactoring

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/handler/BackgroundTaskHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/handler/BackgroundTaskHandler.java
@@ -7,29 +7,31 @@ import android.os.Message;
 
 import androidx.annotation.NonNull;
 
+import java.lang.ref.WeakReference;
+
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.BackgroundTask;
 import edu.byu.cs.tweeter.client.model.service.observer.ServiceObserver;
 
 public abstract class BackgroundTaskHandler<T extends ServiceObserver> extends Handler {
 
-    private final T observer;
+    private final WeakReference<T> observer;
 
     public BackgroundTaskHandler(T observer) {
         super(Looper.getMainLooper());
-        this.observer = observer;
+        this.observer = new WeakReference<T>(observer);
     }
 
     @Override
     public void handleMessage(@NonNull Message msg) {
         boolean success = msg.getData().getBoolean(BackgroundTask.SUCCESS_KEY);
         if (success) {
-            handleSuccessMessage(observer, msg.getData());
+            handleSuccessMessage(observer.get(), msg.getData());
         } else if (msg.getData().containsKey(BackgroundTask.MESSAGE_KEY)) {
             String message = msg.getData().getString(BackgroundTask.MESSAGE_KEY);
-            observer.handleFailure(message);
+            observer.get().handleFailure(message);
         } else if (msg.getData().containsKey(BackgroundTask.EXCEPTION_KEY)) {
             Exception ex = (Exception) msg.getData().getSerializable(BackgroundTask.EXCEPTION_KEY);
-            observer.handleException(ex);
+            observer.get().handleException(ex);
         }
     }
 
